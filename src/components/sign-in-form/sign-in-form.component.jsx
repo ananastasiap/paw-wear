4e99e196a86b1f3d1f6fa04526d4070096cec05f
ananastasiap <anestheziia@gmail.com>
2023-08-18 23:15:32 +0300

    refactor: add auth listener using onAuthStateChange

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,10 +1,9 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import { signInGooglePopup,
          createUserDocumentFromAuth,
          signInAuthUserWithEmailAndPassword, } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
-import { UserContext } from "../../contexts/user.context";
 
 import './sign-in-form.style.scss';
 
@@ -17,15 +16,12 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
-  const { setCurrentUser } = useContext(UserContext);
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInGooglePopup();
-    await createUserDocumentFromAuth(user);
+    await signInGooglePopup();
   }
 
   const handleSubmit = async (event) => {
@@ -34,7 +30,6 @@ const SignInForm = () => {
     try {
       const { user } = await signInAuthUserWithEmailAndPassword(email, password);
 
-      setCurrentUser(user);
       resetFormFields();
     } catch(error) {
       switch(error.code) {
