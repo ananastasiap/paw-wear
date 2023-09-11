8055b6c69429039f6275cea231c17e0f26e5e797
ananastasiap <anestheziia@gmail.com>
2023-09-11 21:59:21 +0300

    refactor: all components style

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { signInGooglePopup,
-         // createUserDocumentFromAuth,
          signInAuthUserWithEmailAndPassword, } from "../../utils/firebase/firebase.utils";
+
 import FormInput from "../form-input/form-input.component";
+
 import Button, {BUTTON_TYPES_CLASSES} from "../button/button.component";
 
-import './sign-in-form.style.scss';
+import { SignInContainer, ButtonsContainer } from "./sign-in-form.style";
 
 const defaultFormFields = {
   email: '',
@@ -29,9 +30,8 @@ const SignInForm = () => {
 
     try {
       const { user } = await signInAuthUserWithEmailAndPassword(email, password);
-      // ! delete ---
+      // ! delete
       console.log(user);
-      // ! ----------
       resetFormFields();
     } catch(error) {
       switch(error.code) {
@@ -54,7 +54,7 @@ const SignInForm = () => {
   };
 
   return (
-    <div className="sign-up-container">
+    <SignInContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -76,12 +76,12 @@ const SignInForm = () => {
           value={password}
         />
 
-        <div className="buttons-container">
+        <ButtonsContainer>
         <Button type='submit'>Sign In</Button>
         <Button type='button' buttonType={BUTTON_TYPES_CLASSES.google} onClick={signInWithGoogle}>Google Sign In</Button>
-        </div>
+        </ButtonsContainer>
       </form>
-    </div>
+    </SignInContainer>
   );
 };
 
