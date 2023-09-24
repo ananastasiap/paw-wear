849338ca1cf612a0388bc2681baa9a52aa228ba4
ananastasiap <anestheziia@gmail.com>
2023-09-24 17:23:43 +0300

    feat: setting up a store

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -20,6 +20,10 @@ export const USER_ACTION_TYPES = {
   SET_CURRENT_USER: 'SET_CURRENT_USER'
 };
 
+const INITIAL_STATE = {
+  currentUser: null
+};
+
 const userReducer = (state, action) => {
   const { type, payload } = action;
 
@@ -32,10 +36,6 @@ const userReducer = (state, action) => {
     default:
       throw new Error(`Unhandled type ${type} in userReducer`);
   }
-};
-
-const INITIAL_STATE = {
-  currentUser: null
 };
 
 export const UserProvider = ({ children }) => {
