2e2b4d5f0bdb91ca5f69be8de7ff9762e2204ab3
ananastasiap <anestheziia@gmail.com>
2023-09-26 17:15:35 +0300

    refactor: categories

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -1,11 +1,13 @@
 import { USER_ACTION_TYPES } from "./user.types";
-// import { createAction } from "../../utils/reducer/reducer.utils";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
-export const setCurrentUser = (user) => ({
-  type: USER_ACTION_TYPES.SET_CURRENT_USER,
-  payload: user,
-});
+// export const setCurrentUser = (user) => ({
+//   type: USER_ACTION_TYPES.SET_CURRENT_USER,
+//   payload: user,
+// });
 
-// при использовании createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user); -- возникает ошибка Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'type')
+export const setCurrentUser = (user) =>
+  createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 
-
+// при использовании createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user); -- возникает ошибка
+// Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'type')
