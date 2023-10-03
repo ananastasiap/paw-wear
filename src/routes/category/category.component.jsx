b89cd37916445d7dbf4a2d980a16e2bea539187a
ananastasiap <anestheziia@gmail.com>
2023-10-03 16:05:55 +0300

    feat: add redux-thunk + spinner

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,14 +3,19 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap
+} from '../../store/categories/category.selector';
 
 import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -20,11 +25,14 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toLocaleUpperCase()}</Title>
-        <CategoryContainer>
-          { products &&
+      {
+        isLoading ? (<Spinner />) : (
+          <CategoryContainer>
+            { products &&
             products.map((product) => <ProductCard key={product.id} product={product} />)
-          }
-        </CategoryContainer>
+            }
+          </CategoryContainer>
+      )}
     </Fragment>
   );
 };
