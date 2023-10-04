bc7bc5e6885fecf1db5c9e64749fc49b1fd09b83
ananastasiap <anestheziia@gmail.com>
2023-10-04 15:40:14 +0300

    refactor: store/user

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -1,13 +1,33 @@
 import { USER_ACTION_TYPES } from "./user.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
 
-// export const setCurrentUser = (user) => ({
-//   type: USER_ACTION_TYPES.SET_CURRENT_USER,
-//   payload: user,
-// });
-
 export const setCurrentUser = (user) =>
   createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 
-// при использовании createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user); -- возникает ошибка
-// Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'type')
+/*
+
+  CHECK_USER_SESSION: 'user/CHECK_USER_SESSION',
+  GOOGLE_SIGN_IN_START: 'user/EMAIL_SIGN_IN_START',
+  EMAIL_SIGN_IN_START: 'user/EMAIL_SIGN_IN_START',
+  SIGN_IN_SUCCESS: 'user/SIGN_IN_SUCCESS',
+  SIGN_IN_FAILURE: 'user/SIGN_IN_FAILURE',
+
+*/
+
+export const checkUserSession = () =>
+  createAction(USER_ACTION_TYPES.CHECK_USER_SESSION);
+
+export const googleSignInStart = () =>
+  createAction(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START);
+
+export const emailSignInStart = (email, password) =>
+  createAction(
+    USER_ACTION_TYPES.EMAIL_SIGN_IN_START,
+    {email, password}
+  );
+
+export const signInSuccess = (user) =>
+    createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user);
+
+export const signInFailed = (error) =>
+    createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error);
