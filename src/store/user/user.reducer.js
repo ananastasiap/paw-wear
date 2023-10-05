ebd4ba1b4aa9ef0a4fa34cac4167d3f79a8d01a5
ananastasiap <anestheziia@gmail.com>
2023-10-05 11:07:19 +0300

    refactor: user sign out using saga

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -6,20 +6,18 @@ const INITIAL_STATE = {
   error: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action = {}) => {
+export const userReducer = (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
 
-  switch(type) {
+  switch (type) {
     case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-      return {
-        ...state,
-        currentUser: payload,
-      };
+      return { ...state, currentUser: payload };
+    case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
+      return { ...state, currentUser: null };
+    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
     case USER_ACTION_TYPES.SIGN_IN_FAILED:
-      return {
-        ...state,
-        error: payload,
-      };
+    case USER_ACTION_TYPES.SIGN_UP_FAILED:
+      return { ...state, error: payload };
     default:
       return state;
   }
