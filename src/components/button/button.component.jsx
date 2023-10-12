90c5e9e7c8292e0ff04a103ef54292bc1bbb9463
ananastasiap <anestheziia@gmail.com>
2023-10-12 21:05:25 +0300

    feat: loading button

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,4 +1,9 @@
-import { BaseButton, GoogleSignInButton, InvertedButton} from './button.styles.jsx';
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+  ButtonSpinner,
+} from './button.styles.jsx';
 
 export const BUTTON_TYPES_CLASSES = {
   base: 'base',
@@ -14,12 +19,12 @@ const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) => (
   }[buttonType]
 )
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
 
   return (
-    <CustomButton {...otherProps}>
-      { children }
+    <CustomButton disabled={isLoading} {...otherProps}>
+      { isLoading ? <ButtonSpinner /> : children }
     </CustomButton>
   )
 }
