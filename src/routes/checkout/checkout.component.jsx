986f74e243f7f7bdfe07eb3d85ee8e50dd2e2346
ananastasiap <anestheziia@gmail.com>
2023-10-12 16:59:01 +0300

    feat: create payment form component / netlify - create payment intent

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import {
   CheckoutContainer,
@@ -36,8 +37,9 @@ const Checkout = () => {
       </CheckoutHeader>
         { cartItems.map((cartItem) => (
               <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
-            ))}
-          <Total>Total: ${cartTotal}</Total>
+        ))}
+        <Total>Total: ${cartTotal}</Total>
+        <PaymentForm />
     </CheckoutContainer>
   );
 };
